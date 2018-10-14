799158eb9565b23322adb8ba3c51f7ceef228d02
Leandro.Carolino <carolino.leandro@gmail.com>
2018-10-14 20:43:38 +0200

    redux provider and store set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
 
 import Posts from './components/Posts'
 import PostForm from './components/PostForm';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-        <PostForm />
-        <hr/>
-        <Posts />
-        
-        <hr/>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+          </header>
+          <PostForm />
+          <hr/>
+          <Posts />
+          
+          <hr/>
+        </div>
+      </Provider>
     );
   }
 }
